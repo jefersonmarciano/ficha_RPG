dadca1c75cd7f1c2336d0fa95ee8cc60fbc2fe21
Jeferson marciano <44215511+jefersonmarciano@users.noreply.github.com>
2024-09-17 03:11:17 -0300

    correção de satyle incones centrais

--- mochila.js
+++ mochila.js
@@ -7,27 +7,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     botao.onclick = function() {
         popup.style.display = "block";
+        carregarItensMochila();
     }
 
     fechar.onclick = function() {
         popup.style.display = "none";
+        salvarItensMochila();
     }
 
     window.onclick = function(event) {
         if (event.target == popup) {
             popup.style.display = "none";
+            salvarItensMochila();
         }
     }
 
     adicionarItem.onclick = function() {
+        adicionarNovoItem("Novo Item", "1", "Descrição do novo item");
+        salvarItensMochila();
+    }
+
+    function adicionarNovoItem(nome, quantidade, descricao) {
         var novaLinha = tabela.insertRow(-1);
         var celula1 = novaLinha.insertCell(0);
         var celula2 = novaLinha.insertCell(1);
         var celula3 = novaLinha.insertCell(2);
         var celula4 = novaLinha.insertCell(3);
-        celula1.innerHTML = '<input type="text" value="Novo Item">';
-        celula2.innerHTML = '<input type="text" value="1">';
-        celula3.innerHTML = '<input type="text" value="Descrição do novo item">';
+        celula1.innerHTML = `<input type="text" value="${nome}">`;
+        celula2.innerHTML = `<input type="text" value="${quantidade}">`;
+        celula3.innerHTML = `<input type="text" value="${descricao}">`;
         celula4.innerHTML = '<span class="remover-item">X</span>';
         adicionarEventoRemover(celula4.firstChild);
     }
@@ -36,11 +44,45 @@ document.addEventListener('DOMContentLoaded', function() {
         elemento.onclick = function() {
             var linha = this.parentNode.parentNode;
             linha.parentNode.removeChild(linha);
+            salvarItensMochila();
         }
     }
 
+    function salvarItensMochila() {
+        var itens = [];
+        var linhas = tabela.getElementsByTagName("tr");
+        for (var i = 1; i < linhas.length; i++) { // Começa em 1 para pular o cabeçalho
+            var celulas = linhas[i].getElementsByTagName("td");
+            var item = {
+                nome: celulas[0].getElementsByTagName("input")[0].value,
+                quantidade: celulas[1].getElementsByTagName("input")[0].value,
+                descricao: celulas[2].getElementsByTagName("input")[0].value
+            };
+            itens.push(item);
+        }
+        localStorage.setItem('itensMochila', JSON.stringify(itens));
+    }
+
+    function carregarItensMochila() {
+        var itensString = localStorage.getItem('itensMochila');
+        if (itensString) {
+            var itens = JSON.parse(itensString);
+            // Limpa a tabela antes de carregar os itens
+            while (tabela.rows.length > 1) {
+                tabela.deleteRow(1);
+            }
+            itens.forEach(function(item) {
+                adicionarNovoItem(item.nome, item.quantidade, item.descricao);
+            });
+        }
+    }
+
+    // Adiciona eventos de remover para itens existentes
     var botoesRemover = document.getElementsByClassName("remover-item");
     for (var i = 0; i < botoesRemover.length; i++) {
         adicionarEventoRemover(botoesRemover[i]);
     }
+
+    // Carrega os itens da mochila ao iniciar a página
+    carregarItensMochila();
 });
